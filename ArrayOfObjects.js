126804e7b9bc79e6811a50d5af86ec6c8ff3a709
unknown <aprajput331998@gmail.com>
2023-08-03 22:39:07 +0530

    Completed Objects and ArrayOfObjects Topic

---
+++ ArrayOfObjects.js
@@ -0,0 +1,24 @@
+
+const arr = [85, 50, 96, 23]
+
+const car = [
+    {
+        model: "Nexon",
+        brand: "Tata"
+    },
+    {
+        fname: "Akash",
+        lname: "Rajput"
+    },
+    {
+        state: "Maharashtra",
+        country: "India"
+    },
+    {
+        indoorGame: "Chess",
+        outdoorGame: "Hockey"
+    }
+]
+
+console.log(car[0].model);
+console.log(car[2].country)
---
+++ Objects.js
@@ -0,0 +1,24 @@
+
+const person = {
+    fname: "Akash",
+    lname: "Rajput",
+    age: 25,
+    city: "Nashik",
+    isAdult: true,
+    hobbies: ["Coding", "Eating", "Playing games"]
+}
+
+person.fname = "Nandkishor"
+
+console.log(person.fname);
+
+// console.log(Object.keys(person))
+// console.log(Object.values(person))
+
+
+// console.log(person["fname"]);
+// console.log(person["lname"]);
+// console.log(person["age"]);
+// console.log(person.city);
+// console.log(person.isAdult);
+// console.log(person.hobbies[0]);
